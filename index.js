12c554cb6ef6fb1d29bd87a28ceade117d39b873
wanderer-s <ows0871@gmail.com>
2023-11-25 23:39:15 +0900

    Update: 처리효율을 위한 잔고 확인 순서 변경
    - 구매수량 매개변수 처리
    - JSdoc 추가

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 import { chromium } from 'playwright';
 import "dotenv/config"
 
-async function purchaseLotto() {
-  const browser = await chromium.launch({headless : true})
+/**
+ * @param {number} purchaseQuantity - 로또 구매 수량
+ */
+async function purchaseLotto(purchaseQuantity) {
+  const browser = await chromium.launch()
   const page = await browser.newPage()
   await page.goto("https://dhlottery.co.kr/user.do?method=login")
   await page.getByPlaceholder("아이디").fill(process.env.ID)
   await page.getByPlaceholder("비밀번호").fill(process.env.PASSWORD)
   await page.getByRole("group").getByRole("link", {name: "로그인"}).click()
 
-  await page.goto("https://ol.dhlottery.co.kr/olotto/game/game645.do")
-  const balanceStr = await page.locator("#moneyBalance").textContent()
-  const balance = parseInt(balanceStr.replace(",",""))
+  const [balanceStr] = (await page.locator("body > div:nth-child(1) > header > div.header_con > div.top_menu > form > div > ul.information > li.money > a:nth-child(2) > strong").textContent()).split("원")
 
-  if(balance === 0) {
+  if(parseInt(balanceStr) === 0) { // 잔액부족으로 구매 할 수 없는 상황
     await browser.close()
+    return
   }
 
+  await page.goto("https://ol.dhlottery.co.kr/olotto/game/game645.do")
   await page.getByText("자동번호발급").click()
-  await page.selectOption("select", String(1))
+  await page.selectOption("select", String(purchaseQuantity))
   await page.getByRole("button", { name: "확인"}).click()
   await page.getByRole("button", { name: "구매하기"}).click()
   await page.locator("#popupLayerConfirm").getByRole("button", { name: "확인" }).click()
@@ -28,4 +31,4 @@ async function purchaseLotto() {
   await browser.close()
 }
 
-await purchaseLotto()
+await purchaseLotto(1)

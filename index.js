430b274a0585f460546143d6ccec7cdd67a9ba01
wanderer-s <ows0871@gmail.com>
2023-12-15 19:23:57 +0900

    balance 확인 주석처리

--- index.js
+++ index.js
@@ -16,10 +16,8 @@ async function purchaseLotto(purchaseQuantity) {
     await page.getByPlaceholder("비밀번호").fill(process.env.PASSWORD)
     await page.getByRole("group").getByRole("link", {name: "로그인"}).click()
 
-    const balanceElement = await page.locator("body > div:nth-child(1) > header > div.header_con > div.top_menu > form > div > ul.information > li.money > a:nth-child(2) > strong").textContent({timeout: 60000})
-    const balance = parseInt(balanceElement.replace(",",""))
-
-    await sendMessage(`TEST: 잔고는 ${balance} 원 입니다`)
+    // const balanceElement = await page.locator("body > div:nth-child(1) > header > div.header_con > div.top_menu > form > div > ul.information > li.money > a:nth-child(2) > strong").textContent()
+    // const balance = parseInt(balanceElement.replace(",",""))
 
     //  if(balance === 1000) {
     //    await sendMessage("잔액을 충전하지 않으면 다음 회차부터 구매할 수 없습니다")
@@ -31,19 +29,20 @@ async function purchaseLotto(purchaseQuantity) {
     //    return
     //  }
     //
-    //  await page.goto("https://ol.dhlottery.co.kr/olotto/game/game645.do")
-    //  await page.getByText("자동번호발급").click()
-    //  await page.selectOption("select", String(purchaseQuantity))
-    //  await page.getByRole("button", { name: "확인"}).click()
-    //  await page.getByRole("button", { name: "구매하기"}).click()
-    //  await page.locator("#popupLayerConfirm").getByRole("button", { name: "확인" }).click()
-    //
-    //  await page.locator("#report").getByRole("button", { name: "확인"}).click()
-    //  const restOfBalanceStr = await page.locator("#moneyBalance").textContent()
-    //  const restOfBalance = parseInt(restOfBalanceStr.replace(",",""))
-    //
-    //  await sendMessage(`${purchaseQuantity}개 로또 구매 완료
-    // 남은 잔고는 ${restOfBalance}원`)
+
+    // local에서는 문제가 없었으나 github actions 에서는 계속해서 timeour 발생하여 주석처리
+    // timeout 시간을 늘려도 해결되지 않았음
+
+     await page.goto("https://ol.dhlottery.co.kr/olotto/game/game645.do")
+     await page.getByText("자동번호발급").click()
+     await page.selectOption("select", String(purchaseQuantity))
+     await page.getByRole("button", { name: "확인"}).click()
+     await page.getByRole("button", { name: "구매하기"}).click()
+     await page.locator("#popupLayerConfirm").getByRole("button", { name: "확인" }).click()
+
+     await page.locator("#report").getByRole("button", { name: "확인"}).click()
+
+     await sendMessage(`${purchaseQuantity}개 로또 구매 완료`)
   } catch (err) {
     console.error(err.message)
     await sendMessage(`Error: ${err.message}`)

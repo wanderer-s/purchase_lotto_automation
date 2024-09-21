7a2e74ef9ed9f3da585646a53de94ed6ba8fe1ad
wanderer-s <ows0871@gmail.com>
2024-09-21 20:29:51 +0900

    test

--- index.js
+++ index.js
@@ -30,37 +30,38 @@ const purchaseLotto = async (purchaseQuantity) => {
     await page.getByRole("group").getByRole("link", {name: "로그인"}).click({timeout: 1000})
     console.log('login success')
 
-    const moneyText = await page.textContent('li.money')
-    const balanceText = moneyText.replace(/[예치금충전출]/g,'').trim()
-    const balance = parseInt(balanceText.replace(",",""))
-
-    console.log(balance)
-
-    if (balance > 5000) {
-      await sendMessage('잔액이 5000보다 많아요')
-      return
-    }
-
-    if(balance === 1000) {
-      await sendMessage("잔액을 충전하지 않으면 다음 회차부터 구매할 수 없습니다")
-    }
-
-    if(balance === 0) {
-      await sendMessage("잔액이 부족하여 로또 구매를 종료합니다")
-      await browser.close()
-    }
-
-    // await page.goto(LOTTO_PURCHASE_URL)
+    // const moneyText = await page.textContent('li.money')
+    // const balanceText = moneyText.replace(/[예치금충전출]/g,'').trim()
+    // const balance = parseInt(balanceText.replace(",",""))
     //
-    // await page.getByRole("link", {name: "자동번호발급"}).click()
-    // await page.selectOption("select", String(purchaseQuantity))
-    // await page.getByRole("button", { name: "확인"}).click()
-    // await page.getByRole("button", { name: "구매하기"}).click()
+    // console.log(balance)
     //
-    // await page.locator("#popupLayerConfirm").getByRole("button", { name: "확인" }).click()
+    // if (balance > 5000) {
+    //   await sendMessage('잔액이 5000보다 많아요')
+    //   return
+    // }
     //
-    // await page.locator("#report").getByRole("button", { name: "확인"}).click()
-    // await sendMessage(`${purchaseQuantity}개 로또 구매 완료. 잔액: ${balance - 1000}`)
+    // if(balance === 1000) {
+    //   await sendMessage("잔액을 충전하지 않으면 다음 회차부터 구매할 수 없습니다")
+    // }
+    //
+    // if(balance === 0) {
+    //   await sendMessage("잔액이 부족하여 로또 구매를 종료합니다")
+    //   await browser.close()
+    // }
+
+    await page.goto(LOTTO_PURCHASE_URL, { waitUntil:'domcontentloaded'})
+    console.log('init to lotto purchase page')
+    
+    await page.getByRole("link", {name: "자동번호발급"}).click()
+    await page.selectOption("select", String(purchaseQuantity))
+    await page.getByRole("button", { name: "확인"}).click()
+    await page.getByRole("button", { name: "구매하기"}).click()
+
+    await page.locator("#popupLayerConfirm").getByRole("button", { name: "확인" }).click()
+
+    await page.locator("#report").getByRole("button", { name: "확인"}).click()
+    await sendMessage(`${purchaseQuantity}개 로또 구매 완료.`)
   } catch (err) {
     console.error(err.message)
     await sendMessage(`Error: ${err.message}`)
